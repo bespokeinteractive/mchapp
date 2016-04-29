c6fef0cac14706be75a8f4b078735217ae4ee5f9
geoff-wasilwa <geoffreywanjala@gmail.com>
2016-04-29 12:26:01 +0300

    Update ObsRequestParser and ANC Triage Fragment controller

--- omod/src/main/java/org/openmrs/module/mchapp/ObsRequestParser.java
+++ omod/src/main/java/org/openmrs/module/mchapp/ObsRequestParser.java
@@ -3,8 +3,8 @@ package org.openmrs.module.mchapp;
 import org.apache.commons.lang.StringUtils;
 import org.openmrs.Concept;
 import org.openmrs.Obs;
+import org.openmrs.Patient;
 import org.openmrs.api.context.Context;
-import org.openmrs.ui.framework.SimpleObject;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,18 +14,18 @@ import java.util.Map;
  * Created by qqnarf on 4/28/16.
  */
 public class ObsRequestParser {
-    public static List<Obs> parseRequest(Map<String, String[]> postedParameters)
+    public static List<Obs> parseRequest(Patient patient, Map<String, String[]> postedParameters) throws Exception
     {
         List<Obs> observations = new ArrayList<Obs>();
 
         for (Map.Entry<String, String[]> postedParameter :
                 (postedParameters.entrySet())) {
-
             if (StringUtils.contains(postedParameter.getKey(), "concept.")) {
                 String obsConceptUuid = postedParameter.getKey().substring("concept.".length());
                 Concept obsConcept = Context.getConceptService().getConceptByUuid(obsConceptUuid);
                 if (postedParameter.getValue().length > 0) {
                     ObsProcessor obsProcessor = ObsFactory.getObsProcessor(obsConcept);
+                    observations.addAll(obsProcessor.createObs(obsConcept, postedParameter.getValue(), patient));
                 }
             }
         }
--- omod/src/main/java/org/openmrs/module/mchapp/fragment/controller/AntenatalTriageFragmentController.java
+++ omod/src/main/java/org/openmrs/module/mchapp/fragment/controller/AntenatalTriageFragmentController.java
@@ -2,29 +2,36 @@ package org.openmrs.module.mchapp.fragment.controller;
 
 import org.openmrs.Obs;
 import org.openmrs.Patient;
+import org.openmrs.api.context.Context;
+import org.openmrs.module.mchapp.MchMetadata;
 import org.openmrs.module.mchapp.ObsRequestParser;
+import org.openmrs.module.mchapp.api.MchService;
 import org.openmrs.ui.framework.SimpleObject;
 import org.openmrs.ui.framework.page.PageRequest;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by qqnarf on 4/27/16.
  */
 public class AntenatalTriageFragmentController {
     public void controller() {
-
     }
+    @SuppressWarnings("unchecked")
     public SimpleObject saveAntenatalTriageInformation(@RequestParam("patientId") Patient patient, PageRequest request) {
         SimpleObject saveStatus = null;
         List<Obs> observations = new ArrayList<Obs>();
         try {
-            observations = ObsRequestParser.passRequest(request.getRequest().getParameterMap());
+            observations = ObsRequestParser.parseRequest(patient, (Map<String,String[]>)request.getRequest().getParameterMap());
         } catch (Exception e) {
             saveStatus = SimpleObject.create("status", "error", "message", e.getMessage());
         }
+
+        Context.getService(MchService.class).saveMchEncounter(patient, observations, MchMetadata._MchProgram.ANC_PROGRAM);
+
         saveStatus = SimpleObject.create("status", "success", "message", "Triage information has been saved.");
         return saveStatus;
     }
