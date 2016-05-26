2e86d16386e5e476df294ab34897c43b7f08e924
Geof Wasilwa <geoffreywanjala@gmail.com>
2016-05-26 21:54:56 +0300

    Refactor CWC Triage page, remove duplicate send patient to MCH Clinic feature

--- omod/src/main/java/org/openmrs/module/mchapp/fragment/controller/CwcTriageFragmentController.java
+++ omod/src/main/java/org/openmrs/module/mchapp/fragment/controller/CwcTriageFragmentController.java
@@ -2,20 +2,15 @@ package org.openmrs.module.mchapp.fragment.controller;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
-import org.apache.commons.lang.StringUtils;
-import org.openmrs.Concept;
 import org.openmrs.Obs;
 import org.openmrs.Patient;
 import org.openmrs.api.context.Context;
 import org.openmrs.module.appui.UiSessionContext;
-import org.openmrs.module.hospitalcore.PatientQueueService;
-import org.openmrs.module.hospitalcore.model.OpdPatientQueue;
 import org.openmrs.module.mchapp.MchMetadata;
 import org.openmrs.module.mchapp.ObsParser;
 import org.openmrs.module.mchapp.SendForExaminationParser;
@@ -30,7 +25,6 @@ import org.springframework.web.bind.annotation.RequestParam;
  * Created by USER on 5/4/2016.
  */
 public class CwcTriageFragmentController {
-    private static final String PATIENT_SPECIAL_CLINIC = "MCH CLINIC";
 
     public void controller(FragmentModel model, UiUtils ui) {
         model.addAttribute("internalReferralSources", SimpleObject.fromCollection(Referral.getInternalReferralOptions(), ui, "label", "id"));
@@ -56,59 +50,9 @@ public class CwcTriageFragmentController {
             }
         }
 
-        String examination = request.getParameter("send_for_examination");
-        if (StringUtils.isNotEmpty(examination) && examination.equals("yes")) {
-            //TODO refer to MCH Clinic
-            Concept referralSpecialClinicConcept = Context.getConceptService().getConcept(PATIENT_SPECIAL_CLINIC);
-            String paymentCategory = patient.getAttribute(14).getValue();
-            sendPatientToOPDQueue(patient, referralSpecialClinicConcept, true, paymentCategory);
-        }
-
         Context.getService(MchService.class).saveMchEncounter(patient, observations, Collections.EMPTY_LIST, Collections.EMPTY_LIST, MchMetadata._MchProgram.CWC_PROGRAM, null);
 
         return SimpleObject.create("status", "success", "message", "Triage information has been saved.");
 
-
-    }
-
-    /**
-     * Send patient for OPD Queue
-     *
-     * @param patient
-     * @param selectedOPDConcept
-     * @param revisit
-     */
-    public void sendPatientToOPDQueue(Patient patient, Concept selectedOPDConcept, boolean revisit, String selectedCategory) {
-        Concept visitStatus = null;
-        if (!revisit) {
-            visitStatus = Context.getConceptService().getConcept("NEW PATIENT");
-        } else {
-            visitStatus = Context.getConceptService().getConcept("REVISIT");
-        }
-
-        OpdPatientQueue queue = Context.getService(PatientQueueService.class).getOpdPatientQueue(
-                patient.getPatientIdentifier().getIdentifier(), selectedOPDConcept.getConceptId());
-        if (queue == null) {
-            queue = new OpdPatientQueue();
-            queue.setUser(Context.getAuthenticatedUser());
-            queue.setPatient(patient);
-            queue.setCreatedOn(new Date());
-            queue.setBirthDate(patient.getBirthdate());
-            queue.setPatientIdentifier(patient.getPatientIdentifier().getIdentifier());
-            queue.setOpdConcept(selectedOPDConcept);
-            queue.setOpdConceptName(selectedOPDConcept.getName().getName());
-            if (null != patient.getMiddleName()) {
-                queue.setPatientName(patient.getGivenName() + " " + patient.getFamilyName() + " " + patient.getMiddleName());
-            } else {
-                queue.setPatientName(patient.getGivenName() + " " + patient.getFamilyName());
-            }
-            //queue.setReferralConcept(referralConcept);
-            //queue.setReferralConceptName(referralConcept.getName().getName());
-            queue.setSex(patient.getGender());
-            queue.setCategory(selectedCategory);
-            queue.setVisitStatus(visitStatus.getName().getName());
-            PatientQueueService queueService = Context.getService(PatientQueueService.class);
-            queueService.saveOpdPatientQueue(queue);
-        }
     }
 }
