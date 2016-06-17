dbbe85ea9662f185a94ef95cf46239e42c979a07
Dennys Henry <daugmaud@gmail.com>
2016-06-17 12:21:05 +0300

    PNC program posting encounter type as ANC

--- omod/src/main/java/org/openmrs/module/mchapp/fragment/controller/PostnatalExaminationFragmentController.java
+++ omod/src/main/java/org/openmrs/module/mchapp/fragment/controller/PostnatalExaminationFragmentController.java
@@ -80,7 +80,7 @@ public class PostnatalExaminationFragmentController {
             }
         }
 
-        Encounter encounter = Context.getService(MchService.class).saveMchEncounter(patient, observations, drugOrders, testOrders, MchMetadata._MchProgram.ANC_PROGRAM, session.getSessionLocation());
+        Encounter encounter = Context.getService(MchService.class).saveMchEncounter(patient, observations, drugOrders, testOrders, MchMetadata._MchProgram.PNC_PROGRAM, session.getSessionLocation());
         QueueLogs.logOpdPatient(patientQueue, encounter);
 
         return SimpleObject.create("status", "success", "message", "Triage information has been saved.");
