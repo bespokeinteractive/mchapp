f6ac3f74f488b1448563575422fea29d460b5e37
Geof Wasilwa <geoffreywanjala@gmail.com>
2016-08-04 09:50:48 +0300

    Refactor and format code

--- omod/src/main/java/org/openmrs/module/mchapp/fragment/controller/CwcTriageFragmentController.java
+++ omod/src/main/java/org/openmrs/module/mchapp/fragment/controller/CwcTriageFragmentController.java
@@ -1,10 +1,29 @@
 package org.openmrs.module.mchapp.fragment.controller;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import javax.servlet.http.HttpServletRequest;
 
 import org.apache.commons.lang.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.openmrs.*;
+import org.openmrs.Concept;
+import org.openmrs.ConceptAnswer;
+import org.openmrs.Encounter;
+import org.openmrs.Obs;
+import org.openmrs.Patient;
+import org.openmrs.PatientProgram;
+import org.openmrs.PatientState;
+import org.openmrs.ProgramWorkflow;
+import org.openmrs.ProgramWorkflowState;
 import org.openmrs.api.ProgramWorkflowService;
 import org.openmrs.api.context.Context;
 import org.openmrs.module.appui.UiSessionContext;
@@ -24,143 +43,128 @@ import org.openmrs.ui.framework.fragment.FragmentConfiguration;
 import org.openmrs.ui.framework.fragment.FragmentModel;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.servlet.http.HttpServletRequest;
-
-import java.text.DateFormat;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.*;
-
 /**
  * Created by USER on 5/4/2016.
  */
 public class CwcTriageFragmentController {
 
-    private int visitTypeId;
     protected final Log log = LogFactory.getLog(getClass());
-    DateFormat ymdDf = new SimpleDateFormat("yyyy-MM-dd");
 
     public void controller(FragmentModel model, FragmentConfiguration config, UiUtils ui,
-    		@RequestParam(value = "encounterId",required=false) String encounterId) {
-    	
-    	if(StringUtils.isNotEmpty(encounterId)){
-    		model.addAttribute("weightCategoryValue", "");
-    		Encounter current =Context.getEncounterService().getEncounter(Integer.parseInt(encounterId));
-    	      
-    	      //pull teh encounter id- from the url
-    	      
-    	        Set<Obs> obs= Context.getEncounterService().getEncounterByUuid(current.getUuid()).getAllObs();
-    	        	for(Obs s:obs){
-    	        		switch(s.getConcept().getId()){
-    	        		case 5089:{
-    	        			model.addAttribute("weight", s.getValueNumeric());
-    	        			break;
-    	        		}
-    	        		case 5090:{
-    	        			model.addAttribute("height", s.getValueNumeric());
-    	        			break;
-    	        		}
-    	        		case 5712:{
-    	        			model.addAttribute("muac", s.getValueNumeric());
-    	        			break;
-    	        		}
-    	        		case 1854:{
-    	        			Concept wCat=s.getValueCoded();
-    	        			model.addAttribute("weightCategoryValue", wCat.getUuid());
-    	        			break;
-    	        		}
-    	        		case 100126186:{
-    	        			Concept gStat=s.getValueCoded();
-    	        			model.addAttribute("growthStatusValue", gStat.getUuid());
-    	        			break;
-    	        		}
-    	        	}
-    	        }
-    	} else {
-    		model.addAttribute("weightCategoryValue", "");
-    		model.addAttribute("growthStatusValue", "");
-    		model.addAttribute("muac", "");
-    		model.addAttribute("height", "");
-    		model.addAttribute("weight", "");
-    		
-    	}
-    	
-    	
-    	
-    	
-    	
-        model.addAttribute("internalReferralSources", SimpleObject.fromCollection(Referral.getInternalReferralOptions(), ui, "label", "id"));
+            @RequestParam(value = "encounterId", required = false) String encounterId) {
 
-        Concept  growthCategory= Context.getConceptService().getConceptByUuid(MchMetadata._MchProgram.MCH_GROWTH_MONITOR);
+        if (StringUtils.isNotEmpty(encounterId)) {
+            model.addAttribute("weightCategoryValue", "");
+            Encounter current = Context.getEncounterService().getEncounter(Integer.parseInt(encounterId));
+
+            Set<Obs> obs = Context.getEncounterService().getEncounterByUuid(current.getUuid()).getAllObs();
+            for (Obs s : obs) {
+                switch (s.getConcept().getId()) {
+                case 5089: {
+                    model.addAttribute("weight", s.getValueNumeric());
+                    break;
+                }
+                case 5090: {
+                    model.addAttribute("height", s.getValueNumeric());
+                    break;
+                }
+                case 5712: {
+                    model.addAttribute("muac", s.getValueNumeric());
+                    break;
+                }
+                case 1854: {
+                    Concept wCat = s.getValueCoded();
+                    model.addAttribute("weightCategoryValue", wCat.getUuid());
+                    break;
+                }
+                case 100126186: {
+                    Concept gStat = s.getValueCoded();
+                    model.addAttribute("growthStatusValue", gStat.getUuid());
+                    break;
+                }
+                }
+            }
+        } else {
+            model.addAttribute("weightCategoryValue", "");
+            model.addAttribute("growthStatusValue", "");
+            model.addAttribute("muac", "");
+            model.addAttribute("height", "");
+            model.addAttribute("weight", "");
+
+        }
+
+        model.addAttribute("internalReferralSources",
+                SimpleObject.fromCollection(Referral.getInternalReferralOptions(), ui, "label", "id"));
+
+        Concept growthCategory = Context.getConceptService()
+                .getConceptByUuid(MchMetadata._MchProgram.MCH_GROWTH_MONITOR);
         List<SimpleObject> growthCategories = new ArrayList<SimpleObject>();
-        
-        if(growthCategory!=null){
+
+        if (growthCategory != null) {
 
             for (ConceptAnswer answer : growthCategory.getAnswers()) {
-                growthCategories.add(SimpleObject.create("uuid", answer.getAnswerConcept().getUuid(),
-                        "label", answer.getAnswerConcept().getDisplayString()));
+                growthCategories.add(SimpleObject.create("uuid", answer.getAnswerConcept().getUuid(), "label",
+                        answer.getAnswerConcept().getDisplayString()));
             }
         }
         model.addAttribute("growthCategories", growthCategories);
 
-        Concept weightCategory = Context.getConceptService().getConceptByUuid(MchMetadata._MchProgram.MCH_WEIGHT_CATEGORIES);
+        Concept weightCategory = Context.getConceptService()
+                .getConceptByUuid(MchMetadata._MchProgram.MCH_WEIGHT_CATEGORIES);
         List<SimpleObject> weightCategories = new ArrayList<SimpleObject>();
-        if(weightCategory!=null){
+        if (weightCategory != null) {
 
             for (ConceptAnswer answer : weightCategory.getAnswers()) {
-                weightCategories.add(SimpleObject.create("uuid", answer.getAnswerConcept().getUuid(),
-                        "label", answer.getAnswerConcept().getDisplayString()));
+                weightCategories.add(SimpleObject.create("uuid", answer.getAnswerConcept().getUuid(), "label",
+                        answer.getAnswerConcept().getDisplayString()));
             }
         }
 
         model.addAttribute("weightCategories", weightCategories);
-      
 
-      
- }
+    }
 
     @SuppressWarnings("unchecked")
 
-    public SimpleObject saveCwcTriageInfo(
-            @RequestParam("patientId") Patient patient, 
-            @RequestParam("queueId") Integer queueId,
-            @RequestParam("patientEnrollmentDate") Date patientEnrollmentDate,
-            @RequestParam(value="isEdit", required = false) Boolean isEdit,
-            UiSessionContext session,
+    public SimpleObject saveCwcTriageInfo(@RequestParam("patientId") Patient patient,
+            @RequestParam("queueId") Integer queueId, @RequestParam("patientEnrollmentDate") Date patientEnrollmentDate,
+            @RequestParam(value = "isEdit", required = false) Boolean isEdit, UiSessionContext session,
             HttpServletRequest request) {
         PatientQueueService queueService = Context.getService(PatientQueueService.class);
         TriagePatientQueue queue = queueService.getTriagePatientQueueById(queueId);
         List<Obs> observations = new ArrayList<Obs>();
         ObsParser obsParser = new ObsParser();
-        for (Map.Entry<String, String[]> postedParams :
-                ((Map<String, String[]>) request.getParameterMap()).entrySet()) {
+        for (Map.Entry<String, String[]> postedParams : ((Map<String, String[]>) request.getParameterMap())
+                .entrySet()) {
             try {
-                observations = obsParser.parse(
-                        observations, patient, postedParams.getKey(),
-                        postedParams.getValue());
+                observations = obsParser.parse(observations, patient, postedParams.getKey(), postedParams.getValue());
             } catch (Exception e) {
                 return SimpleObject.create("status", "error", "message", e.getMessage());
             }
         }
-        List<Object> previousVisitsByPatient = Context.getService(MchService.class).findVisitsByPatient(patient, true, true, patientEnrollmentDate);
+        List<Object> previousVisitsByPatient = Context.getService(MchService.class).findVisitsByPatient(patient, true,
+                true, patientEnrollmentDate);
+        int visitTypeId;
         if (previousVisitsByPatient.size() == 0) {
             visitTypeId = MchMetadata._MchProgram.INITIAL_MCH_CLINIC_VISIT;
         } else {
             visitTypeId = MchMetadata._MchProgram.RETURN_CWC_CLINIC_VISIT;
         }
-        Encounter encounter = Context.getService(MchService.class).saveMchEncounter(patient, observations, Collections.EMPTY_LIST,
-                Collections.EMPTY_LIST, MchMetadata._MchProgram.CWC_PROGRAM,
+        Encounter encounter = Context.getService(MchService.class).saveMchEncounter(patient, observations,
+                Collections.EMPTY_LIST, Collections.EMPTY_LIST, MchMetadata._MchProgram.CWC_PROGRAM,
                 MchMetadata._MchEncounterType.CWC_TRIAGE_ENCOUNTER_TYPE, session.getSessionLocation(), visitTypeId);
         if (request.getParameter("send_for_examination") != null) {
             String visitStatus = queue.getVisitStatus();
-            SendForExaminationParser.parse("send_for_examination", request.getParameterValues("send_for_examination"), patient, visitStatus);
+            SendForExaminationParser.parse("send_for_examination", request.getParameterValues("send_for_examination"),
+                    patient, visitStatus);
         }
-        
+
         if (!isEdit) {
-        	QueueLogs.logTriagePatient(queue, encounter);
+            QueueLogs.logTriagePatient(queue, encounter);
         }
-        		
-        return SimpleObject.create("status", "success", "message", "Triage information has been saved.", "isEdit",isEdit);
+
+        return SimpleObject.create("status", "success", "message", "Triage information has been saved.", "isEdit",
+                isEdit);
     }
 
     public SimpleObject updatePatientProgram(HttpServletRequest request) {
@@ -170,8 +174,8 @@ public class CwcTriageFragmentController {
             String enrollmentDateYmd = request.getParameter("enrollmentDateYmd");
             String completionDateYmd = request.getParameter("completionDateYmd");
             String outcomeId = request.getParameter("outcomeId");
-            Context.getService(MchService.class).updatePatientProgram(Integer.parseInt(programId),
-                    enrollmentDateYmd, completionDateYmd, null, Integer.parseInt(outcomeId));
+            Context.getService(MchService.class).updatePatientProgram(Integer.parseInt(programId), enrollmentDateYmd,
+                    completionDateYmd, null, Integer.parseInt(outcomeId));
         } catch (ParseException e) {
             return SimpleObject.create("status", "error", "message", e.getMessage());
         }
@@ -188,8 +192,8 @@ public class CwcTriageFragmentController {
         for (PatientState st : p.statesInWorkflow(wf, false)) {
             ret.add(new PatientStateItem(st));
         }
-        return SimpleObject.fromCollection(ret, uiUtils, "patientStateId", "programWorkflowId", "stateName", "workflowName",
-                "startDate", "endDate","dateCreated","creator");
+        return SimpleObject.fromCollection(ret, uiUtils, "patientStateId", "programWorkflowId", "stateName",
+                "workflowName", "startDate", "endDate", "dateCreated", "creator");
     }
 
     public List<SimpleObject> getPossibleNextStates(HttpServletRequest request, UiUtils uiUtils) {
@@ -219,7 +223,8 @@ public class CwcTriageFragmentController {
         Date onDate = null;
         if (onDateDMY != null && onDateDMY.length() > 0) {
             try {
-                onDate = ymdDf.parse(onDateDMY);
+                DateFormat bigEndianDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+                onDate = bigEndianDateFormat.parse(onDateDMY);
                 pp.transitionToState(st, onDate);
                 s.savePatientProgram(pp);
             } catch (ParseException e) {
