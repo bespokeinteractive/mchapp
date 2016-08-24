64c32527354c1fcc61c68bf66defa74ddc4630be
stansL <mstans8@gmail.com>
2016-08-24 13:45:37 +0300

    add ORMs and HBMs for the immunization commodities functionality

---
+++ api/src/main/java/org/openmrs/module/mchapp/db/hibernate/HibernateImmunizationCommoditiesDAO.java
@@ -0,0 +1,11 @@
+package org.openmrs.module.mchapp.db.hibernate;
+
+import org.openmrs.module.inventory.db.hibernate.HibernateInventoryDAO;
+import org.openmrs.module.mchapp.db.ImmunizationCommoditiesDAO;
+
+/**
+ * @author Stanslaus Odhiambo
+ * Created on 8/24/2016.
+ */
+public class HibernateImmunizationCommoditiesDAO extends HibernateInventoryDAO implements ImmunizationCommoditiesDAO{
+}
