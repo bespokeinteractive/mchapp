074e027eb2cd585ff4f810a60586c8e746841952
stansL <mstans8@gmail.com>
2016-08-24 13:45:37 +0300

    add ORMs and HBMs for the immunization commodities functionality

---
+++ api/src/main/java/org/openmrs/module/mchapp/db/ImmunizationCommoditiesDAO.java
@@ -0,0 +1,16 @@
+package org.openmrs.module.mchapp.db;
+
+import org.openmrs.module.inventory.db.InventoryDAO;
+
+/**
+ * @author Stanslaus Odhiambo
+ * Create on 8/24/2016.
+ * Interface that declares the functionalities to be implemented by the implementing class that enters into a contract
+ * with it
+ */
+public interface ImmunizationCommoditiesDAO extends InventoryDAO {
+
+//    TODO Behaviours for implementing the Immunization functionalities
+    
+
+}
